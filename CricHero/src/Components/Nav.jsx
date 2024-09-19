9a8c96ca10ce2b4d95fa3d05cd6f003f0bad82e3
Dhaval Bathe <omkarbathe4@gmail.com>
2024-09-19 18:55:57 +0530

    changes made in nav.module.css for headingStyle where .  class operator is missing

--- CricHero/src/Components/Nav.jsx
+++ CricHero/src/Components/Nav.jsx
@@ -1,7 +1,10 @@
+import styles from "./Nav.module.css";
+
+
 export const Nav = () => {
     return (
         <>
-            <h1>This is the navbar Section</h1>
+            <h1 className={styles.headingStyle}>This is the navbar Section</h1>
         </>
     )
 }
