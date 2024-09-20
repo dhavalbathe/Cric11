ffeb36145807550c50ac523b2c354091fa040781
Dhaval Bathe <omkarbathe4@gmail.com>
2024-09-20 13:18:30 +0530

    Changes added in Scrolling header

--- CricHero/src/Components/ScrollingText.jsx
+++ CricHero/src/Components/ScrollingText.jsx
@@ -1,11 +1,16 @@
 import styles from "./ScrollingText.module.css";
+import ball from "../assets/cricket ball.png";
 
 export const ScrollingText = () => {
     return (
         <>
             <div className={styles["scrolling-Header"]}>
                 <div className={styles["scrolling-Text"]}>
-                    Cric 11  |  Cric 11  |  Cric 11  
+                    <div className={styles["text-container"]}>
+                        <img className={styles["cricket-ball"]} src={ball} alt="cricket ball" /> <span className={styles["text"]}>
+                        Cric 11  |  Cric 11  |  Cric 11</span> 
+                        <img className={styles["cricket-ball"]} src={ball} alt="cricket ball" /> 
+                    </div> 
                 </div>
             </div>
         </>
