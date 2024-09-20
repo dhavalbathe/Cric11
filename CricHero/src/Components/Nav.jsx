d7e262c8dd98cca486b3c3791b33be470e241798
100xEngineerai <vedantshinde501@gmail.com>
2024-09-20 19:52:25 +0530

    Nav bar added

--- CricHero/src/Components/Nav.jsx
+++ CricHero/src/Components/Nav.jsx
@@ -1,10 +1,30 @@
-import styles from "./Nav.module.css";
-
+import React from 'react';
+import styles from './Nav.module.css';
 
 export const Nav = () => {
-    return (
-        <>
-            <h1 className={styles.headingStyle}>This is the navbar Section</h1>
-        </>
-    )
-}
+  return (
+    <>
+      <nav className={styles.navbar}>
+        <div className={styles.navContainer}>
+          <a href="#" className={styles.logoContainer}>
+            <div className={styles.logoText}>CRIC 11</div>
+          </a>
+          <ul className={styles.navLinks}>
+            <li><a href="#" className={styles.navLink}>Home</a></li>
+            <li><a href="#" className={styles.navLink}>Services</a></li>
+            <li><a href="#" className={styles.navLink}>Tournaments</a></li>
+            <li><a href="#" className={styles.navLink}>About Us</a></li>
+            <li><a href="#" className={styles.navLink}>Contact</a></li>
+          </ul>
+          <div className={styles.mobileMenu}>
+            <button className={styles.menuButton}>
+              <svg xmlns="http://www.w3.org/2000/svg" className={styles.menuIcon} fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" />
+              </svg>
+            </button>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+};
