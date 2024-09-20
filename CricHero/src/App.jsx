27d835606b3be208fc96ad4659ddc0fcbb12ba07
Dhaval Bathe <omkarbathe4@gmail.com>
2024-09-20 11:09:54 +0530

    Added The feature of horizontally scrolling Text

--- CricHero/src/App.jsx
+++ CricHero/src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { ScrollingText } from "./Components/ScrollingText.jsx";
 import { Nav } from "./Components/Nav.jsx";
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
@@ -9,6 +10,7 @@ function App() {
 
   return (
     <>
+      <ScrollingText />
       <Nav />
     </>
   )
---
+++ CricHero/src/Components/ScrollingText.jsx
@@ -0,0 +1,13 @@
+import styles from "./ScrollingText.module.css";
+
+export const ScrollingText = () => {
+    return (
+        <>
+            <div className={styles["scrolling-Header"]}>
+                <div className={styles["scrolling-Text"]}>
+                    Cric 11  |  Cric 11  |  Cric 11  
+                </div>
+            </div>
+        </>
+    )
+}
