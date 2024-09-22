bf36f40eb924b61b17a5cbdfad7b536453269268
Chandrahas10 <chandrahaskori9@gmail.com>
2024-09-22 13:34:37 +0530

    upload

--- CricHero/src/App.jsx
+++ CricHero/src/App.jsx
@@ -1,12 +1,6 @@
 import { useState } from 'react';
 import { ScrollingText } from "./Components/ScrollingText.jsx";
 import { Nav } from "./Components/Nav.jsx";
-<<<<<<< HEAD
-// import {First} 
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-=======
->>>>>>> 7effd03e29e3723280ead7de2c2570828d5b8fa3
 import './App.css'
 
 function App() {
@@ -14,21 +8,11 @@ function App() {
 
   return (
     <>
-<<<<<<< HEAD
-        <div className="hero-section">
-            <ScrollingText />
-            <Nav />
-        </div>
-        <main className ="firstpage">
-          <First />
-        </main>
-=======
           <ScrollingText />
           <Nav />
           <div className="hero-section">
             
           </div>
->>>>>>> 7effd03e29e3723280ead7de2c2570828d5b8fa3
     </>
   )
 }
