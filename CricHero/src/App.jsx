da468d753c9e0de779b2bcb9e878b10b3868da4d
100xEngineerai <vedantshinde501@gmail.com>
2024-09-22 14:59:36 +0530

    app.jsx

--- CricHero/src/App.jsx
+++ CricHero/src/App.jsx
@@ -12,13 +12,22 @@ function App() {
           <ScrollingText />
           <Nav />
           <div className="hero-section">
-<<<<<<< HEAD
-            <div className="left-section">
+              <div className="left-section">
+                <div className="text">
+                    
+                </div>
+                <div className="matches">
+                    <Match />
+                    <Match />
+                </div>
+
+              </div>
+          <div className="right-section">
               <div className="text">
                   </div>
               <div className="matches"></div>
-            </div>
-            <div className="right-section">
+              </div>
+              <div className="right-section">
               <h1>Digital Score Card</h1>
               <div className="match-type"></div>
               <div className="search-match"></div>
@@ -28,26 +37,14 @@ function App() {
                 <div className="ind1">IND</div>
                 <div className="ind-score">257/7</div>
                 <div className="ind-over">19 over</div>
-=======
-              <div className="left-section">
-                <div className="text">
-
-                </div>
-                <div className="matches">
-                    <Match />
-                    <Match />
-                </div>
-
->>>>>>> 9a82dd8f5195bc57196ce4fce547eb48f21f2cb8
               </div>
               <div className="pak">
                 <div className="pak1">PAK</div>
                 <div className="pak-score">261/9</div>
                 <div className="pak-over">20 over</div>
               </div>
-              
-            </div>
-          </div>
+              </div>
+        </div>
     </>
   )
 }
