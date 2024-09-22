9ede9977b252b78ef4979e61e412a0ee5b455166
Chandrahas10 <chandrahaskori9@gmail.com>
2024-09-22 13:09:52 +0530

    upload

--- CricHero/src/App.jsx
+++ CricHero/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import { ScrollingText } from "./Components/ScrollingText.jsx";
 import { Nav } from "./Components/Nav.jsx";
+// import {First} 
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -14,6 +15,9 @@ function App() {
             <ScrollingText />
             <Nav />
         </div>
+        <main className ="firstpage">
+          <First />
+        </main>
     </>
   )
 }
