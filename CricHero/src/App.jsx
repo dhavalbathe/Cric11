29cb63b1b4772e3e840db427e2e583ff3caa5b00
Dhaval Bathe <omkarbathe4@gmail.com>
2024-09-22 20:36:42 +0530

    changes made

--- CricHero/src/App.jsx
+++ CricHero/src/App.jsx
@@ -4,10 +4,6 @@ import { Nav } from "./Components/Nav.jsx";
 import { Match } from "./Components/Match.jsx";
 import cricketer from "./assets/cricketer.png";
 import './App.css'
-import SocialMediaSection from './Components/SocialMediaSection.jsx';
-import '@fortawesome/fontawesome-free/css/all.min.css';
-
-import './App.css';
 
 function App() {
   
@@ -51,27 +47,6 @@ function App() {
                 <div className="ind1">IND</div>
                 <div className="ind-score">257/7</div>
                 <div className="ind-over">19 over</div>
-      <ScrollingText />
-      <Nav />
-      <div className="hero-section">
-        <div className="left-section">
-          <div className="text">
-            {/* Additional content can be added here */}
-          </div>
-          <div className="matches">
-            <Match />
-            <Match />
-          </div>
-        </div>
-        <div className="right-section">
-          <h1>Digital Score Card</h1>
-          <span className="search-type">
-            <span>
-              <p className="matchtype">Match type</p>
-              <div className="match-type">
-                <button className="option-button active">T-20</button>
-                <button className="option-button">ODI</button>
-                <button className="option-button">T-10</button>
               </div>
               <div className="pak">
                 <div className="pak1">PAK</div>
@@ -80,40 +55,8 @@ function App() {
               </div>
               </div>
           </div>
-          </span>
-         <SocialMediaSection/>
-           
-
-            <span>
-              <p>Search Your match</p>
-              <div className="search-container">
-                <input
-                  type="text"
-                  placeholder="Search..."
-                  className="search-input"
-                />
-                <button className="search-button">Search</button>
-              </div>
-            </span>
-          </span>
-          <div class="ind-pak">
-            <p className="match-between">IND VS PAK</p>
-            <div className="ind">
-              <p className="ind1">IND</p>
-              <p className="ind-score">257/7</p>
-              <p className="ind-over">19 over</p>
-            </div>
-            <div className="pak">
-              <p className="pak1">PAK</p>
-              <p className="pak-score">261/9</p>
-              <p className="pak-over">20 over</p>
-            </div>
-          </div>
-        </div>
-      </div>
     </>
-  );
+  )
 }
 
 export default App;
-
