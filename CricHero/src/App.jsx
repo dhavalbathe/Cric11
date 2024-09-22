660caabfa8de31887fa82733cbb2db7349b82079
Chandrahas10 <chandrahaskori9@gmail.com>
2024-09-22 18:59:43 +0530

    changes

--- CricHero/src/App.jsx
+++ CricHero/src/App.jsx
@@ -3,8 +3,9 @@ import { ScrollingText } from "./Components/ScrollingText.jsx";
 import { Nav } from "./Components/Nav.jsx";
 import { Match } from "./Components/Match.jsx";
 import cricketer from "./assets/cricketer.png"; // Importing cricketer image
-import indiaFlag from "./assets/india.webp"; // Import the Indian flag image
+import indiaFlag from "./assets/indiaflag.webp"; // Import the Indian flag image
 import pakFlag from "./assets/pakflag.webp"; // Import the Pakistani flag image
+import logo1 from "./assets/logo.png"; 
 import './App.css';
 import SocialMediaSection from './Components/SocialMediaSection.jsx';
 import '@fortawesome/fontawesome-free/css/all.min.css';
@@ -79,15 +80,16 @@ function App() {
             {/* Score and Overs Details */}
             <div className="score-section">
               <div className="team-score ind">
-                <p className="team-label">IND</p>
-                <p className="score">257/7</p>
+                <p className="team-label">IND  <img className="team-flag" src={indiaFlag} alt="pakflag" /></p>
+                <p className="score">257/2</p>
                 <p className="overs">19 overs</p>
                 <p className="current-batting">
-                  <strong>Current Batsmen:</strong> Rohit Sharma (65 off 34)
+                  <strong>Current Batsmen:</strong> Rohit S(65 off 54)
+                   Ruturaj G( 26 off 26)
                 </p>
               </div>
               <div className="team-score pak">
-                <p className="team-label">PAK</p>
+                <p className="team-label">PAK <img className="team-flag" src={pakFlag} alt="pakflag" /></p>
                 <p className="score">261/9</p>
                 <p className="overs">20 overs</p>
                 <p className="current-bowling">
