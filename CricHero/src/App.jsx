11e3de69ba502f604a0938e8951cdf742bcd3920
Chandrahas10 <chandrahaskori9@gmail.com>
2024-09-22 14:23:53 +0530

    upload

--- CricHero/src/App.jsx
+++ CricHero/src/App.jsx
@@ -11,18 +11,29 @@ function App() {
           <ScrollingText />
           <Nav />
           <div className="hero-section">
-              <div className="left-section">
-                <div className="text">
-
-                </div>
-                <div className="matches">
-                  
-                </div>
-
+            <div className="left-section">
+              <div className="text">
+                  </div>
+              <div className="matches"></div>
+            </div>
+            <div className="right-section">
+              <h1>Digital Score Card</h1>
+              <div className="match-type"></div>
+              <div className="search-match"></div>
+             
+              <div className="match-between">IND VS PAK</div>
+              <div className="ind">
+                <div className="ind1">IND</div>
+                <div className="ind-score">257/7</div>
+                <div className="ind-over">19 over</div>
               </div>
-              <div className="right-section">
-
+              <div className="pak">
+                <div className="pak1">PAK</div>
+                <div className="pak-score">261/9</div>
+                <div className="pak-over">20 over</div>
               </div>
+              
+            </div>
           </div>
     </>
   )
